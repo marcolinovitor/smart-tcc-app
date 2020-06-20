c0b9e4433e79fea8e28f635ddd67774a6b5597eb
Vitor Marcolino <marcolino.vitor@gmail.com>
2020-06-20 15:10:35 -0300

    splash screen

---
+++ src/app/shared/back-button.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-back-button',
+    templateUrl: 'name.component.html'
+})
+
+export class BackButton implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+    static 
+}
